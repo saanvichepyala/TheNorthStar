57099754385dbf289bfc298827f80882ddc63bf7
saanvichepyala <saanvichepyala@gmail.com>
2026-07-20 09:47:16 -0500

    Add files via upload

---
+++ North Star/North Star/North_StarApp.swift
@@ -0,0 +1,17 @@
+//
+//  North_StarApp.swift
+//  North Star
+//
+//  Created by Saanvi chepyala on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct North_StarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ North Star/northstar/northstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  northstarApp.swift
+//  northstar
+//
+//  Created by Saanvi chepyala on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct northstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
